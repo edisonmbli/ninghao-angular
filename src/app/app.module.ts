0c5317354fd2bfe05088c844305e6e1682472f67
edisonmbli <edisonmbli@gmail.com>
2020-06-28 00:29:19 +0800

    styles enhancement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 
 import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, PostModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
